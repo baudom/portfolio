2815d43e9effe1feb00875baafe5614eb4f67a73
Dominik Baurecht <dominik@baurecht-welden.de>
2026-02-05 11:38:31 +0100

    ui improvements

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -7,78 +7,81 @@ import { WithChildren } from "@/types/react";
 import { BrandTagType } from "@/types/tag";
 
 export type ProjectCardLinkProps = {
-    title: string;
-    icon: ForwardRefExoticComponent<IconProps>;
-    href: string;
+  title: string;
+  icon: ForwardRefExoticComponent<IconProps>;
+  href: string;
 };
 
 export type ProjectCardProps = WithChildren & {
-    className?: string;
-    icon?: ReactNode;
-    title: string;
-    tags?: BrandTagType[];
-    links?: ProjectCardLinkProps[];
+  className?: string;
+  icon?: ReactNode;
+  title: string;
+  tags?: BrandTagType[];
+  links?: ProjectCardLinkProps[];
 };
 
 const ProjectCard: FC<ProjectCardProps> = ({
-    className,
-    icon,
-    title,
-    children,
-    tags,
-    links,
+  className,
+  icon,
+  title,
+  children,
+  tags,
+  links,
 }) => {
-    return (
-        <Card className={cn("flex flex-col md:h-full", className)}>
-            <div className="flex justify-between">
-                <div className="skewed-reverse text-card-heading flex items-center gap-2">
-                    <div className="flex h-12 w-12 items-center justify-center rounded-xl skewed bg-glass text-card-highlight shadow-sm backdrop-blur-sm">
-                        <div className="skewed-reverse">{icon}</div>
-                    </div>
-                    <h3>{title}</h3>
-                </div>
-                {links?.length ? (
-                    <div className="skewed-reverse flex gap-4">
-                        {links.map((link) => (
-                            <IconButton
-                                key={link.title}
-                                name={link.title}
-                                href={link.href}
-                            >
-                                <link.icon size="1.25rem" />
-                            </IconButton>
-                        ))}
-                    </div>
-                ) : null}
+  return (
+    <Card
+      className={cn(
+        "flex flex-col md:h-full lg:!skew-x-0", // "!" needed to overwrite skewed class
+        className,
+      )}
+    >
+      <div className="flex justify-between">
+        <div className="skewed-reverse text-card-heading flex items-center gap-2">
+          <div className="flex h-12 w-12 items-center justify-center rounded-xl skewed bg-glass text-card-highlight">
+            <div className="skewed-reverse">{icon}</div>
+          </div>
+          <h3>{title}</h3>
+        </div>
+        {links?.length ? (
+          <div className="skewed-reverse flex gap-4">
+            {links.map((link) => (
+              <IconButton
+                key={link.title}
+                name={link.title}
+                href={link.href}
+              >
+                <link.icon size="1.25rem" />
+              </IconButton>
+            ))}
+          </div>
+        ) : null}
+      </div>
+      <div className="flex-1 text-sm md:text-base text-card-body mt-4">
+        {children}
+      </div>
+      {tags?.length ? (
+        <div className="flex flex-wrap gap-2 md:gap-4 mt-4">
+          {tags.map((t) => (
+            <div
+              key={t.name}
+              className="skewed-reverse"
+            >
+              <div className="skewed px-2 py-1 rounded-xl bg-tag flex items-center gap-2 select-none">
+                <t.icon
+                  size="1.25rem"
+                  className="skewed-reverse"
+                  color={t.color}
+                />
+                <p className="text-xs md:text-sm font-mono skewed-reverse">
+                  {t.name}
+                </p>
+              </div>
             </div>
-            <div className="flex flex-col md:flex-row">
-                <div className="flex-1 text-sm md:text-base text-card-body">
-                    {children}
-                </div>
-                {tags?.length ? (
-                    <div className="flex-1 flex flex-wrap gap-x-2 md:gap-x-4 gap-y-2 md:gap-y-4 mt-4 md:mt-0 md:justify-center md:content-center">
-                        {tags.map((t) => (
-                            <div
-                                key={t.name}
-                                className="skewed-reverse"
-                            >
-                                <div className="skewed px-2 py-1 rounded-xl bg-tag flex items-center gap-2 select-none">
-                                    <t.icon
-                                        size="1.25rem"
-                                        className="skewed-reverse"
-                                        color={t.color}
-                                    />
-                                    <p className="text-xs md:text-sm font-mono skewed-reverse">
-                                        {t.name}
-                                    </p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                ) : null}
-            </div>
-        </Card>
-    );
+          ))}
+        </div>
+      ) : null}
+    </Card>
+  );
 };
 
 export default memo(ProjectCard);
--- src/components/SectionContainer/index.tsx
+++ src/components/SectionContainer/index.tsx
@@ -5,36 +5,30 @@ import { cn } from "@/utils/tailwind";
 import Title, { TitleProps } from "@/components/Title";
 
 type SectionContainerProps = WithChildren &
-    WithClassName & {
-        id: AnchorType;
-        title?: TitleProps;
-        hasFullWidthChildren?: boolean;
-    };
+  WithClassName & {
+    id: AnchorType;
+    title?: TitleProps;
+  };
 
 const WIDTH_CLASSES = "w-11/12 md:w-9/12";
 // NOTE: keep ~aligned with Carousel width
 
 const SectionContainer: FC<SectionContainerProps> = ({
-    id,
-    className,
-    children,
-    title,
-    hasFullWidthChildren = false,
+  id,
+  className,
+  children,
+  title,
 }) => (
-    <section
-        id={id.replace("#", "")}
-        className={cn(
-            "min-h-screen place-content-center justify-items-center",
-            className,
-        )}
-    >
-        <div className={WIDTH_CLASSES}>
-            {title ? <Title {...title} /> : null}
-        </div>
-        <div className={cn(hasFullWidthChildren ? "w-full" : WIDTH_CLASSES)}>
-            {children}
-        </div>
-    </section>
+  <section
+    id={id.replace("#", "")}
+    className={cn(
+      "min-h-screen place-content-center justify-items-center",
+      className,
+    )}
+  >
+    <div className={WIDTH_CLASSES}>{title ? <Title {...title} /> : null}</div>
+    <div className={WIDTH_CLASSES}>{children}</div>
+  </section>
 );
 
 export default SectionContainer;
